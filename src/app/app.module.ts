e3db4aeacafdad29684cb815b7bdf7a3af6675d5
Florin Tomozei <florin.tomozei@spyhce.com>
2018-03-12 20:51:47 +0200

    first modules + base routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { HttpClientModule } from '@angular/common/http';
 
-
+import { MaterialModule } from './material.module';
+import { AppRoutingModule } from './app.routing';
 import { AppComponent } from './app.component';
 
 
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+
+const appRoutes: Routes = [];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(appRoutes, { enableTracing: false }) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
